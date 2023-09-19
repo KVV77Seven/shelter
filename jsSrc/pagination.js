965dbfa1e6919b8a285182e2ef8842bb2773ae6a
Kadatskikh Vladislav notebook <kvv77seven@mail.ru>
2023-09-19 13:21:05 +0600

    pagination dev end

--- jsSrc/pagination.js
+++ jsSrc/pagination.js
@@ -1,13 +1,21 @@
 import {breakpoints, currentBreakpoint} from "shelter/jsSrc/breakpoints";
-import {getCardById} from "shelter/jsSrc/petCardGetter";
+import {getCardById, getCardsCount} from "shelter/jsSrc/petCardGetter";
 
 const container = document.getElementById('pagination__cards-container');
 
-let currentCards = 0
+let currentCards = 0;
+let currentIndex = 0;
 const countCards = {
     sm: 0,
     md: 0,
     lg: 0
+}
+const paginators = {
+    moveLeft: document.getElementById('move-left'),
+    jumpLeft: document.getElementById('jump-left'),
+    moveRight: document.getElementById('move-right'),
+    jumpRight: document.getElementById('jump-right'),
+    page: document.getElementById('pagination-page'),
 }
 
 export function init(
@@ -21,10 +29,13 @@ export function init(
     setCurrentCardsFromBp(currentBreakpoint.current);
     window.addEventListener('breakpointChange', onBpChange);
 
-    for (let i = 0; i < currentCards; i++) {
-        const {card} = getCardById(i);
-        container.appendChild(card);
-    }
+    paginators.moveLeft.addEventListener('click', moveLeft)
+    paginators.moveRight.addEventListener('click', moveRight)
+    paginators.jumpLeft.addEventListener('click', jumpLeft)
+    paginators.jumpRight.addEventListener('click', jumpRight)
+
+    updateCardsOnPage()
+    updateBtnsState()
 
 }
 
@@ -47,6 +58,76 @@ function setCurrentCardsFromBp(breakpoint) {
     }
 }
 
+function updateCardsOnPage() {
+    container.innerHTML = '';
+    let i = currentIndex === 0 ? 0 : currentIndex*currentCards;
+    for (let j = i; j < i+currentCards && j<getCardsCount(); j++) {
+        const {card} = getCardById(j);
+        container.appendChild(card);
+    }
+}
+
 function onBpChange(e) {
     setCurrentCardsFromBp(e.detail);
+    updateCardsOnPage();
+    updateBtnsState();
+}
+
+function moveRight() {
+    if (paginators.moveRight.disabled) {
+        return;
+    }
+    console.log(paginators.moveRight.disabled)
+    currentIndex++
+    updateBtnsState()
+    updateCardsOnPage()
+}
+function jumpRight() {
+    if (paginators.jumpRight.disabled) {
+        return;
+    }
+    currentIndex = Math.ceil(getCardsCount()/currentCards)-1; 
+    updateBtnsState()
+    updateCardsOnPage()
+
+
+}
+function moveLeft() {
+    if (paginators.moveLeft.disabled) {
+        return;
+    }
+    currentIndex--
+    updateCardsOnPage()
+    updateBtnsState()
+}
+
+function jumpLeft() {
+    if (paginators.jumpLeft.disabled) {
+        return;
+    }
+    currentIndex = 0; 
+    updateCardsOnPage()
+    updateBtnsState()
+}
+function updateBtnsState() {
+    if(currentIndex <= 0) {
+        paginators.moveLeft.disabled = true;
+        paginators.jumpLeft.disabled = true;
+        console.log('dis1')
+    } 
+    if(currentIndex > 0) {
+        paginators.moveLeft.disabled = false;
+        paginators.jumpLeft.disabled = false;
+        console.log('dis2')
+    } 
+    if(currentIndex >= Math.ceil(getCardsCount()/ currentCards)-1) {
+        paginators.moveRight.disabled = true;
+        paginators.jumpRight.disabled = true;
+        console.log('dis3')
+    }
+    if (currentIndex < Math.ceil(getCardsCount()/ currentCards)-1) {
+        paginators.moveRight.disabled = false;
+        paginators.jumpRight.disabled = false;
+        console.log('dis4')
+    }
 }
